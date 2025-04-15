5f41ef28c5181aa9e6073caefba1f94089b4fe5c
Mohammad Woafi <woafisun@yahoo.com>
2025-04-15 22:00:55 +0600

    2nd commit

--- app/contact/page.js
+++ app/contact/page.js
@@ -36,9 +36,9 @@ function Contact() {
         transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' },
       }}
       className="py-6">
-      <div className="container mx-auto">
+      <div className="container mx-auto px-5 lg:px-0">
         <div className="flex flex-col lg:flex-row gap-[30px]">
-          <div className="lg:h-[54%] order-2 lg:order-none">
+          <div className="lg:w-[54%] order-2 lg:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl " action="">
               <h3 className="text-4xl accent">Let's work together</h3>
               <p className="text-white/60">
@@ -70,16 +70,16 @@ function Contact() {
             </form>
           </div>
           {/* info */}
-          <div className="flex-1 flex item-center lg:justify-end order-1 lg:order-none mb-8 lg:mb-0">
-            <ul className="flex flex-col gap-10">
+          <div className="flex-2 flex item-center justify-center order-1 lg:order-none mb-8 lg:mb-0">
+            <ul className="flex flex-col gap-10 justify-center">
               {info.map((item, index) => (
                 <li key={index} className="flex items-center gap-6">
                   <div className="w-[52px] h-[52px] lg:w-[72px] lg:h-[72px] bg-[#27272c] accent rounded-md flex items-center justify-center">
-                    <div>{item.icon}</div>
+                    <div className="text-[28px]">{item.icon}</div>
                   </div>
-                  <div>
-                    <p>{item.title}</p>
-                    <h3>{item.description}</h3>
+                  <div className="flex-1">
+                    <p className="text-white/60">{item.title}</p>
+                    <h3 className="text-xl">{item.description}</h3>
                   </div>
                 </li>
               ))}
